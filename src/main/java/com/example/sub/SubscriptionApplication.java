361ae24ca9a644abb2db5941f12f06e2195582de
vagus3 <mohoham3@kangnam.ac.kr>
2026-06-08 18:41:34 +0900

    chore: project 뼈대 구성 및 gradlew wrapper

---
+++ src/main/java/com/example/sub/SubscriptionApplication.java
@@ -0,0 +1,14 @@
+package com.example.sub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class SubscriptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubscriptionApplication.class, args);
+    }
+}
